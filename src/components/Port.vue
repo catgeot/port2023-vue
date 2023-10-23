f7f92d5e6339c4b66a35bdfca933ee98367f0e48
catgeot <catgeot@naver.com>
2023-10-24 08:20:18 +0900

    포트폴이오 완성

--- src/components/Port.vue
+++ src/components/Port.vue
@@ -1,3 +1,154 @@
+<script setup>
+    import { portText } from '../constants';
+</script>
+
 <template>
-    Port
+    <section id="port">
+            <div class="port_inner">
+                <h2 class="port_title">
+                    portfolio <em>포트폴리오 작업물</em>
+                </h2> 
+                <div class="port_wrap">
+                    <article class="port_item p1" v-for="(port, key) in portText" :key="key">
+                        <span class="num">{{port.num}}.</span>
+                        <a :href="port.code" target="_blank" class="img">
+                            <img :src="port.img" :alt="port.name" />
+                        </a>
+                        <h3 class="title">{{port.title}}</h3>
+                        <p class="desc">
+                            {{port.desc}}
+                        </p>
+                        <a :href="port.view" target="_blank" class="site">사이트 보기</a>
+                    </article>
+                </div>   
+            </div>
+        </section>
 </template>
+<style lang="scss">
+    #port {
+    width: 100%;
+    height: 100vh;
+    margin-top: 30vh;
+    overflow: hidden;
+ 
+    .port_inner {
+        padding: 16px;
+
+    }
+    .port_title {
+        position: sticky;
+        width: 100%;
+        height: 5vw;
+        font-size: 4vw;
+        font-weight: 900;
+        line-height: 1.5;
+        font-family: var(--mainKor-font);
+        text-transform: uppercase;
+        color: var(--black100);
+        border-bottom: 0.4vw solid var(--black100);
+        margin-bottom: 20px;
+        text-indent: -0.3vw;  
+        
+        em{
+            font-size: 1.25rem;
+            font-weight: 400;
+            line-height: 2;
+        }
+    }
+    .port_wrap{
+        display: flex;
+        flex-wrap: wrap;
+        width: 7000px;
+
+        .port_item{
+            width: 500px;
+            height: 120vh;
+            background-color: var(--subBg100);
+            padding: 2.5rem;
+            margin-right: 20px;  
+            
+            &.p1{
+                background-color: var(--subBg100);
+            }
+            &.p2{
+                background-color: var(--subBg200);
+            }
+            &.p3{
+                background-color: var(--subBg300);
+            }
+            &.p4{
+                background-color: var(--subBg400);
+            }
+            &.p5{
+                background-color: var(--subBg500);
+            }
+            &.p6{
+                background-color: var(--subBg100);
+            }
+            &.p7{
+                background-color: var(--subBg200);
+            }
+            &.p8{
+                background-color: var(--subBg300);
+            }
+            &.p9{
+                background-color: var(--subBg400);
+            }
+            &.p10{
+                background-color: var(--subBg500);
+            }
+            .num{
+                font-size: 2rem;
+                font-family: var(--mainNum-font);
+            }
+            .img{
+                display: block;
+                margin-top: -20px;
+                transition: margin-top 0.3s;
+
+                img {
+                    border-radius: 10px;
+                    max-width: 500px;
+                    max-height: 35vh;
+                    overflow: hidden;
+                    filter: saturate(0%);
+                    transition: all 0.3s;  
+                }
+
+                &:hover {
+                    margin-top: 0;
+
+                    img {
+                        filter: saturate(100%); 
+                    }
+                }
+            }
+            .title{
+                font-size: 1.5rem;
+                text-align: center;
+                padding: 0.8rem 0;
+                font-weight: 700;
+                color: var(--black100);
+                border-bottom: 2px solid var(--black100);
+                margin-bottom: 1rem;
+            }
+            .desc{
+                font-size: 1rem;
+            } 
+            .site{
+                border: 1px solid var(--black100);
+                display: block;
+                text-align: center;
+                padding: 0.625rem 1.5rem;
+                margin-top: 1.5rem;
+
+                &:hover {
+                    background-color: var(--black100);
+                    color: var(--white);
+                }
+            }           
+        }
+    }
+
+}
+</style>
